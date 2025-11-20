45efb9a44b21935d0b582733269445cea34eaabc
yasin rezaei <97335055+Yasinrz@users.noreply.github.com>
2025-11-20 14:29:03 +0330

    add sms welcom & result labortory

--- laboratory/apps.py
+++ laboratory/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class LaboratoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'laboratory'
+
+    def ready(self):
+        import laboratory.signals
